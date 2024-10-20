91cecdad4730def3d32c8246204c9bab7586a352
Believeharsh <bontinue@gmail.com>
2024-10-20 12:07:01 +0530

    minor style changes on the StarRating

--- src/SmallApps/Star-Rating/StarRating.jsx
+++ src/SmallApps/Star-Rating/StarRating.jsx
@@ -7,12 +7,11 @@ const StarRating = ({ numOfStars = 5 }) => {
   const [hover, sethover] = useState(null);
 
   const handleClick = (currentIndex) => {
-    setrating(currentIndex); 
-   
+    setrating(currentIndex);
   };
 
   const handleMouseEnter = (currentIndex) => {
-    sethover(currentIndex); 
+    sethover(currentIndex);
   };
 
   const handleMouseLeave = () => {
@@ -22,17 +21,17 @@ const StarRating = ({ numOfStars = 5 }) => {
   return (
     <>
       <AppsHeader headertext="Star Rating" />
-      <div className="container mx-auto px-4 py-8 bg-blue-500 rounded-lg shadow-lg mt-3">
-        <div className="flex justify-center items-center space-x-2 p-10">
+      <div className="container mx-auto p-4 bg-blue-500 rounded-lg shadow-lg max-w-md mt-6">
+        <div className="flex justify-center items-center space-x-2 py-6">
           {[...Array(numOfStars)].map((_, index) => {
             index += 1;
             return (
               <FaStar
                 key={index}
-                className={`cursor-pointer text-4xl ${
+                className={`cursor-pointer text-4xl transition-all duration-200 ${
                   index <= (hover || rating) ? 'text-yellow-400' : 'text-gray-800'
                 }`}
-                onClick={() => handleClick(index)} 
+                onClick={() => handleClick(index)}
                 onMouseEnter={() => handleMouseEnter(index)}
                 onMouseLeave={handleMouseLeave}
               />
