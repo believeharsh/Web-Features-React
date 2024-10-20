20f83560cfc3f94d21d3f2febe10d37bb294e311
Believeharsh <bontinue@gmail.com>
2024-10-20 12:02:30 +0530

    added Star Rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import SingleSelection from "./SmallApps/Accordian/SingleSelection";
 import MultipleSelection from "./SmallApps/Accordian/MultipleSelection";
 import RandomColor from "./SmallApps/Random-Color/RandomColor";
+import StarRating from "./SmallApps/Star-Rating/StarRating";
 const App = () => {
   return (
     <>
     {/* <SingleSelection/> */}
     <MultipleSelection/>
     <RandomColor/>
+    <StarRating />
     </>
   )
 };
---
+++ src/SmallApps/Star-Rating/StarRating.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+import AppsHeader from '../../components/AppsHeader';
+
+const StarRating = ({ numOfStars = 5 }) => {
+  const [rating, setrating] = useState(0);
+  const [hover, sethover] = useState(null);
+
+  const handleClick = (currentIndex) => {
+    setrating(currentIndex); 
+   
+  };
+
+  const handleMouseEnter = (currentIndex) => {
+    sethover(currentIndex); 
+  };
+
+  const handleMouseLeave = () => {
+    sethover(rating);
+  };
+
+  return (
+    <>
+      <AppsHeader headertext="Star Rating" />
+      <div className="container mx-auto px-4 py-8 bg-blue-500 rounded-lg shadow-lg mt-3">
+        <div className="flex justify-center items-center space-x-2 p-10">
+          {[...Array(numOfStars)].map((_, index) => {
+            index += 1;
+            return (
+              <FaStar
+                key={index}
+                className={`cursor-pointer text-4xl ${
+                  index <= (hover || rating) ? 'text-yellow-400' : 'text-gray-800'
+                }`}
+                onClick={() => handleClick(index)} 
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default StarRating;
